3d21f1f7b8cab8c1793429827c5c1e770ee631f8
Lex <nxlex87@gmail.com>
2023-10-24 00:58:07 +0300

    fix

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,64 +1,85 @@
-import React from 'react'
-import './Pagination.css'
+import React from 'react';
+import './Pagination.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 type NaviData = {
-  length: number,
-  current: number,
-  paginationHandler(page: number): void
-  rangePages: number
-}
+  length: number;
+  current: number;
+  paginationHandler(page: number): void;
+  rangePages: number;
+};
 
-export const Pagination: React.FC<NaviData> = props => {
-
-  const currentPage = (page:number) => {
-    let classes: string = 'page-item'
+export const Pagination: React.FC<NaviData> = (props) => {
+  const currentPage = (page: number) => {
+    let classes: string = 'page-item';
     if (props.current === page) {
-      classes = 'page-item active'
+      classes = 'page-item active';
     }
-    return classes
-  }
+    return classes;
+  };
 
   const renderNavi = () => {
-    const arr = []
-    const total = Math.ceil(props.length/10)
-    let left = props.current - props.rangePages
-    let right = props.current + props.rangePages
+    const arr: any[] = [];
+    const total = Math.ceil(props.length / 10);
+    let left = props.current - props.rangePages;
+    let right = props.current + props.rangePages;
 
     // left pages + current
-    if ((props.current - props.rangePages) > 1) {
-      arr.push(<li key={'page_1'} className='page-item'><span className='page-link' onClick={()=>changePage(1)}>1</span></li>);
+    if (props.current - props.rangePages > 1) {
+      arr.push(
+        <li key={'page_1'} className="page-item">
+          <span className="page-link" onClick={() => changePage(1)}>
+            1
+          </span>
+        </li>,
+      );
     }
-    for (let j = left; j <= props.current; j++) { 
-      if (j > 0) {          
-        arr.push(<li key={'page_'+j} className={currentPage(j)}><span className='page-link' onClick={()=>changePage(j)}>{j}</span></li>);
+    for (let j = left; j <= props.current; j++) {
+      if (j > 0) {
+        arr.push(
+          <li key={'page_' + j} className={currentPage(j)}>
+            <span className="page-link" onClick={() => changePage(j)}>
+              {j}
+            </span>
+          </li>,
+        );
       }
     }
     // right pages
-    for (let j = (props.current + 1); j <= right; j++) { 
-      if (j <= total) {          
-        arr.push(<li key={'page_'+j} className='page-item'><span className='page-link' onClick={()=>changePage(j)}>{j}</span></li>);
+    for (let j = props.current + 1; j <= right; j++) {
+      if (j <= total) {
+        arr.push(
+          <li key={'page_' + j} className="page-item">
+            <span className="page-link" onClick={() => changePage(j)}>
+              {j}
+            </span>
+          </li>,
+        );
       }
     }
-    if ((props.current + props.rangePages) < total) {
-      arr.push(<li key={'page_'+total} className='page-item'><span className='page-link' onClick={()=>changePage(total)}>{total}</span></li>);
+    if (props.current + props.rangePages < total) {
+      arr.push(
+        <li key={'page_' + total} className="page-item">
+          <span className="page-link" onClick={() => changePage(total)}>
+            {total}
+          </span>
+        </li>,
+      );
     }
 
-    return arr
-  }
-  const changePage = (page:number) => {
-    props.paginationHandler(page)
-  }
+    return arr;
+  };
+  const changePage = (page: number) => {
+    props.paginationHandler(page);
+  };
 
   return (
     <div className="row">
       <div className="Pagination">
-        <nav> 
-          <ul className="pagination pagination-lg">
-            {renderNavi()}
-          </ul>
+        <nav>
+          <ul className="pagination pagination-lg">{renderNavi()}</ul>
         </nav>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/components/TableRow/TableRow.tsx
+++ src/components/TableRow/TableRow.tsx
@@ -1,45 +1,49 @@
-import React from 'react'
-import './TableRow.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import './TableRow.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 type TableData = {
   data: {
     id: number;
-    firstName: string,
-    lastName: string,
-    email: string,
-    phone: string,
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
     address: {
-      streetAddress: string,
-      city: string,
-      state: string,
-      zip: number
-    },
-    description: string
-  },
-  rowInfo(id: number): any
-  num: number,
-  cls: string[]
-}
+      streetAddress: string;
+      city: string;
+      state: string;
+      zip: number;
+    };
+    description: string;
+  };
+  rowInfo(id: number): any;
+  num: number;
+  cls: string[];
+};
 
-export const TableRow: React.FC<TableData> = props => {   
-  const onRowHandler = (row: number) => {	    
-    props.rowInfo(row)   
-  }
+export const TableRow: React.FC<TableData> = (props) => {
+  const onRowHandler = (row: number) => {
+    props.rowInfo(row);
+  };
   return (
     <tr className={props.cls.join(' ')} onClick={() => onRowHandler(props.num)}>
       <th scope="row">{props.data.id}</th>
       <td>{props.data.firstName}</td>
       <td>{props.data.lastName}</td>
       <td>{props.data.email}</td>
-      <td>{props.data.phone}</td>      
+      <td>{props.data.phone}</td>
       <td>
-        {props.data.address.streetAddress}<br/>
-        {props.data.address.city}<br/>
-        {props.data.address.state}<br/>
-        {props.data.address.zip}<br/>
+        {props.data.address.streetAddress}
+        <br />
+        {props.data.address.city}
+        <br />
+        {props.data.address.state}
+        <br />
+        {props.data.address.zip}
+        <br />
       </td>
       <td>{props.data.description}</td>
-    </tr> 
-  )
-}
+    </tr>
+  );
+};
